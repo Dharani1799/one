298f240e42e9c95d5d9652f9aa6b0eecfdec208a
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-18 00:11:35 +0530

    8.c

---
+++ 8.c
@@ -0,0 +1,23 @@
+#include<string.h>
+
+void main()
+{
+	char a[100];
+	int l,i;
+	gets(a);
+	l=strlen(a);
+	for(i=0;i<l;i++)
+	{
+	    if(i==0)
+	    {
+	    a[0]=toupper(a[0]);
+	    }
+	    if(a[i]==' ')
+	    {
+	    a[i+1]=toupper(a[i+1]);
+	    }
+	    
+	}
+	printf("%s",a);
+		
+}
